4668affbff031059e2b3fc6fca07e6755730473c
KobetiakVasyl <kobetiakvasyl@gmail.com>
2021-02-13 16:37:29 +0200

    server configuration

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/todo'));
+app.use(express.static(__dirname + '/dist/church-front-end'));
 
 app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname+'/dist/todo/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/church-front-end/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
