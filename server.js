050a73a2b833da5742cd09576fefeac145893d98
KobetiakVasyl <kobetiakvasyl@gmail.com>
2021-03-06 21:40:31 +0200

    heroku configuration

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/church-front-end'));
+app.use(express.static(__dirname + '/dist/churchFrontEnd'));
 
 app.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname + '/dist/church-front-end/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/churchFrontEnd/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
